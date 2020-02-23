4eff579ec3c407a48fb4ebcccde9ab7fc1def6e8
Romario Oroutsi <romario.oruci@gmail.com>
2020-02-23 02:24:08 +0200

    edited Start

--- Start.php
+++ Start.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
-    <title>Grades</title>
+    <title>Main</title>
 </head>
 <body>
     <main>
